10fbb280acda8445050c5dadef4da5e725f833b6
Alex <acastrillo87@gmail.com>
2025-08-10 21:35:08 -0400

    test(jest): mock AsyncStorage in Jest to avoid native module error

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     'node_modules/(?!(react-native|@react-native|@expo|expo(nent)?|@expo(nent)?/.*|expo-.*|react-clone-referenced-element|react-native-vector-icons|@react-navigation)/)',
   ],
   testMatch: ['**/__tests__/**/*.test.ts?(x)'],
+  setupFiles: ['@react-native-async-storage/async-storage/jest/async-storage-mock'],
 };
 
 
